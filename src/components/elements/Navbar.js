5351df42bed229055ef7be66b93857fb11f48c60
vinothsmart <s.vinothkanna90@gmail.com>
2021-04-01 18:42:19 +0530

    header and footer added

--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -1,17 +1,28 @@
 export const NavBar = () => {
   return (
-    <div>
-      <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark bg-primary">
-        <div class="container">
-          <a class="navbar-brand" href="#">
-            Contact Book
+    <>
+      <nav className="navbar shadow sticky-top  fixed-top navbar-expand-sm navbar-dark bg-primary">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            React Redux Dashboar
           </a>
           <div>
             <a className="btn btn-light ml-auto">Create Contact</a>
           </div>
         </div>
       </nav>
-    </div>
+      <nav className="navbar fixed-bottom navbar-light bg-light">
+        <div className="container-fluid">
+          <div className="col-lg-12">
+            <center>
+              <a class="navbar-brand" href="https://github.com/vinothsmart/">
+                CopyRights
+              </a>
+            </center>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 export default NavBar;
