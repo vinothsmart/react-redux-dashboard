6607de03d9eca4e3fc6217929ec14c5b1813bd72
vinothsmart <s.vinothkanna90@gmail.com>
2021-04-01 17:30:36 +0530

    logo and app.js updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from "./logo.svg";
 import "./styles/App.css";
 import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Home />
-      </header>
+      <Home />
     </div>
   );
 }
