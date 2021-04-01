63df04002d0b3bcb1ac3f4be62d02f62325a800f
vinothsmart <s.vinothkanna90@gmail.com>
2021-04-01 17:04:33 +0530

    Home component added in App

---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1> Welcome to React </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
