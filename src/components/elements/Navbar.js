b28e43c54a4bc9e1c580c73899d2ef383b686c88
vinothsmart <s.vinothkanna90@gmail.com>
2021-04-13 17:48:37 +0530

    Navbar added

--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -36,7 +36,14 @@ export const NavBar = () => {
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Profile
+                  <img
+                    src="https://github.com/mdo.png"
+                    alt="mdo"
+                    width="32"
+                    height="32"
+                    class="rounded-circle me-2"
+                  />
+                  <strong>Vinoth</strong>
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li>
@@ -57,6 +64,84 @@ export const NavBar = () => {
           </div>
         </div>
       </nav>
+      <header class="p-3 mb-3 border-bottom">
+        <div class="container">
+          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a
+              href="/"
+              class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"
+            >
+              Logo
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li>
+                <a href="#" class="nav-link px-2 link-dark">
+                  Dashboard
+                </a>
+              </li>
+              <li>
+                <a href="#" class="nav-link px-2 link-dark">
+                  Roles
+                </a>
+              </li>
+              <li>
+                <a href="#" class="nav-link px-2 link-dark">
+                  Users
+                </a>
+              </li>
+            </ul>
+
+            <div class="dropdown text-end">
+              <a
+                href="#"
+                class="d-block link-dark text-decoration-none dropdown-toggle"
+                id="dropdownUser1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://github.com/mdo.png"
+                  alt="mdo"
+                  width="32"
+                  height="32"
+                  class="rounded-circle"
+                />
+              </a>
+              <ul
+                class="dropdown-menu text-small"
+                aria-labelledby="dropdownUser1"
+              >
+                <li>
+                  <a class="dropdown-item" href="#">
+                    New project...
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider"></hr>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Sign out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <div class="b-example-divider"></div>
       <nav className="navbar fixed-bottom navbar-light bg-light">
         <div className="container-fluid">
           <div className="col-lg-12">
