e93604084b3d374284cb94ebe57519aaaea2baef
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-06 06:43:00 +0530

    old chages

---
+++ src/components/Roles/Roles.js
@@ -0,0 +1,11 @@
+const Roles = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1> Roles Component </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Roles;
