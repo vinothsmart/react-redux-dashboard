0983e1b1d6b89abcfbffee235a24856f966a4604
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-08 06:36:10 +0530

    root reducer added

---
+++ src/reducers/roleReducer.js
@@ -0,0 +1,35 @@
+const initialState = {
+  roles: [],
+  role: null,
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "GET_ROLES":
+      return { ...state, roles: payload };
+    case "CREATE_ROLE":
+      return {
+        ...state,
+        roles: [payload, ...state.roles],
+      };
+    case "GET_ROLE":
+      return {
+        ...state,
+        role: payload,
+      };
+    case "UPDATE_ROLE":
+      return {
+        ...state,
+        roles: state.posts.map((roleItem) =>
+          roleItem.id == payload.id ? payload : roleItem
+        ),
+      };
+    case "DELETE_ROLE":
+      return {
+        ...state,
+        roles: state.posts.filter((roleItem) => roleItem.id != payload),
+      };
+    default:
+      return state;
+  }
+};
