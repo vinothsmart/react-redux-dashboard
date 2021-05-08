f2a3674cd31190237ea6c4a4c83b3a8fa5ca8a13
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-08 07:02:50 +0530

    roles action updated

--- src/actions/roleAction.js
+++ src/actions/roleAction.js
@@ -1,7 +1,23 @@
-export const roleAction = () => {
-  return (
-    <div>
-      <h1>asdad</h1>
-    </div>
-  );
-};
+// get a role
+export const getRole = (id) => ({
+  type: "GET_ROLE",
+  payload: id,
+});
+
+// create a role
+export const createRole = (role) => ({
+  type: "CREATE_ROLE",
+  payload: role,
+});
+
+// update a role
+export const updateRole = (role) => ({
+  type: "UPDATE_ROLE",
+  payload: role,
+});
+
+// delete a role
+export const deleteRole = (id) => ({
+  type: "DELETE_ROLE",
+  payload: id,
+});
