6efc12531259fcc5a19d0676eacb0b52be976b5f
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-08 06:59:06 +0530

    store added to application

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./styles/App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navbar, Home, Test, Testing, Roles } from "./components";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/roles" component={Roles} />
-          <Route exact path="/test" component={Test} />
-          <Route exact path="/testing" component={Testing} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/roles" component={Roles} />
+            <Route exact path="/test" component={Test} />
+            <Route exact path="/testing" component={Testing} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
