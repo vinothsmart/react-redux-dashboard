9ef12053024845497c38b9b27092ea06174d7c64
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-21 16:51:14 +0530

    Dynamic title worked wow

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,9 +1,16 @@
+import { Helmet } from "react-helmet";
+
 export const Testing = () => {
   return (
-    <div className="container">
-      <div className="py-4">
-        <h1> Testing Page</h1>
+    <>
+      <Helmet>
+        <title>Testing Page</title>
+      </Helmet>
+      <div className="container">
+        <div className="py-4">
+          <h1> Testing Page</h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
