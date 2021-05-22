5d40e3fef5a890175d1f544eaea6c1e5c429447f
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-22 10:32:14 +0530

    roles list page table added

--- src/components/pages/Roles/Roles.js
+++ src/components/pages/Roles/Roles.js
@@ -1,13 +1,43 @@
 import { SeoTool } from "../../elements";
+// import { Link } from "react-router-dom";
 
 export const Roles = () => {
   return (
     <>
-      <SeoTool title="Roles List" />
+      <SeoTool title="Roles" />
       <div className="container">
         <div className="py-4">
-          <h1> Roles Component </h1>
+          <h1> Roles List </h1>
         </div>
+        <table className="table table-shadow table-striped">
+          <thead>
+            <tr>
+              <th scope="col">id</th>
+              <th scope="col">Role</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {roles.map((role) => {
+              <tr>
+                <th scope="row">{role.id}</th>
+                <td>{role.name}</td>
+                <td>
+                  <Link to={`/contacts/edit/${role.id}`}>
+                    <button className="btn btn-primary" type="submit">
+                      Edit
+                    </button>
+                  </Link>
+                  <Link>
+                    <button className="btn btn-primary" type="submit">
+                      Delete
+                    </button>
+                  </Link>
+                </td>
+              </tr>;
+            })} */}
+          </tbody>
+        </table>
       </div>
     </>
   );
