0f133f6ce02af26bec2c14632844c9c747be3e12
vinothsmart <s.vinothkanna90@gmail.com>
2021-06-03 16:34:34 +0530

    Add role

--- src/components/pages/Roles/AddRole.js
+++ src/components/pages/Roles/AddRole.js
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import { SeoTool } from "../../elements";
+import { createRole } from "../../../actions/index";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
 
 export const AddRole = () => {
+  let history = useHistory();
+  const [role, setROle] = useState("");
+
+  const dispatch = useDispatch();
+
+  const addRole = (e) => {
+    e.preventDefault();
+    const inputFields = {
+      userRoleId: role,
+      userRole: role,
+    };
+    // console.log(inputFields);
+    dispatch(createRole(inputFields));
+    history.push("/roles");
+  };
+
   return (
     <>
       <SeoTool title="Add Role" />
@@ -9,16 +29,16 @@ export const AddRole = () => {
           <div className="card border-0 shadow">
             <div className="card-header">Add a Role</div>
             <div className="card-body">
-              <form>
+              <form onSubmit={(e) => addRole(e)}>
                 <div className="form-group mb-3">
                   <input
                     type="text"
                     className="form-control"
                     placeholder="role"
-                    //   value={name}
-                    //   onChange={(e) => {
-                    //     setName(e.target.value);
-                    //   }}
+                    value={role}
+                    onChange={(e) => {
+                      setROle(e.target.value);
+                    }}
                   />
                 </div>
                 <button className="btn btn-primary float-start" type="submit">
