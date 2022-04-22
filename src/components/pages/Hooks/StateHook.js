3bd5a12c494f5b05e72e4b5df2cd0f2528a19dd1
vinothsmart <s.vinothkanna90@gmail.com>
2022-04-22 07:49:48 +0530

    useState hook

--- src/components/pages/Hooks/StateHook.js
+++ src/components/pages/Hooks/StateHook.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 
 const StateHook = () => {
-  const [counter, setCounter] = useState(0);
-  const increment = () => {
-    setCounter((prev) => prev + 1);
+  // const [counter, setCounter] = useState(0);
+  // const increment = () => {
+  //   setCounter((prev) => prev + 1);
+  // };
+
+  const [inputValue, setInputValue] = useState("Vinoth");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
   };
   return (
     <div>
-      {counter}
-      <button onClick={increment}>Increment</button>
+      {/* {counter}
+      <button onClick={increment}>Increment</button> */}
+      <input onChange={handleChange} placeholder="enter something" />
+      {inputValue}
     </div>
   );
 };
