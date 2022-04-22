6a9bc2b019d0611a76433a2576922e6333127ada
vinothsmart <s.vinothkanna90@gmail.com>
2022-04-22 15:22:46 +0530

    Comparison operators, strict equality

--- src/components/pages/JsChallage/Basic.js
+++ src/components/pages/JsChallage/Basic.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Basic = () => {
   const sum = (a, b) => a + b;
+  const compare = (a, b) => a === b;
   return (
     <div>
       <center>
@@ -9,6 +10,13 @@ const Basic = () => {
         <p>{sum(1, 2)}</p>
         <p>{sum(1, 10)}</p>
         <p>{sum(99, 1)}</p>
+      </center>
+      <center>
+        <h1>Comparison operators, strict equality</h1>
+        <p>{compare(2, 3) ? "yes" : "no"}</p>
+        <p>{compare(3, 3) ? "yes" : "no"}</p>
+        <p>{compare(1, "1") ? "yes" : "no"}</p>
+        <p>{compare("10", "10") ? "yes" : "no"}</p>
       </center>
     </div>
   );
