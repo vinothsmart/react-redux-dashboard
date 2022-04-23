c028f2a7e9c7ee6b89278bc734e8108b72afe858
vinothsmart <s.vinothkanna90@gmail.com>
2022-04-23 08:17:16 +0530

    useEffect

--- src/components/pages/Hooks/EffectHook.js
+++ src/components/pages/Hooks/EffectHook.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const EffectHook = () => {
-  return <div>EffectHook</div>;
+  const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/comments").then((res) => {
+      setData(res.data[0].email);
+      console.log("API was Called");
+    });
+  }, [count]);
+  return (
+    <div>
+      <center>
+        Hello
+        <h1>{data}</h1>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>Click Here</button>
+      </center>
+    </div>
+  );
 };
 
 export default EffectHook;
