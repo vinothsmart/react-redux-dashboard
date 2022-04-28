9e4f0eea0d35b39ce97f4ecf03f4e04777005f9b
vinothsmart <s.vinothkanna90@gmail.com>
2022-04-28 09:58:31 +0530

    usememo with single data

--- src/components/pages/Hooks/MemoHook.js
+++ src/components/pages/Hooks/MemoHook.js
@@ -2,31 +2,33 @@ import axios from "axios";
 import React, { useEffect, useMemo, useState } from "react";
 
 const MemoHook = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState("");
   const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/comments").then((res) => {
+    axios.get("https://jsonplaceholder.typicode.com/comments/1").then((res) => {
       setData(res.data);
     });
   }, []);
 
   const findLognestName = (comments) => {
-    if (!comments) return null;
+    // if (!comments) return null;
 
-    let longestName = "";
-    for (let i = 0; i < comments.length; i++) {
-      let currentName = comments[i].name;
-      if (currentName.length > longestName.length) {
-        longestName = currentName;
-      }
-    }
+    // let longestName = "";
+    // for (let i = 0; i < comments.length; i++) {
+    //   let currentName = comments[i].name;
+    //   if (currentName.length > longestName.length) {
+    //     longestName = currentName;
+    //   }
+    // }
     console.log("This was Computed");
 
-    return longestName;
+    // return longestName;
+    return comments;
   };
 
-  const getLongestName = useMemo(() => findLognestName(data), [data]);
+  //   const getLongestName = useMemo(() => findLognestName(data), [data]);
+  const getLongestName = useMemo(() => findLognestName(data.name), [data]);
 
   return (
     <div>
