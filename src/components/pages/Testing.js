d513c38965eee249691cbefd318a5665ca26a6b0
vinothsmart <s.vinothkanna90@gmail.com>
2022-04-30 08:58:16 +0530

    rm curring

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,26 +1,6 @@
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
-  // Function Curring
-  //  add (1,2) => 3
-  //  add (1)(2) => 3 how to write the functon
-
-  // normal func
-  function add(a, b) {
-    return a + b;
-  }
-  console.log(add(1, 2));
-
-  // curring func
-  function curringAdd(a) {
-    // a is accesing from outer scope
-    // closure applided for value of a
-    return function (b) {
-      return a + b;
-    };
-  }
-  console.log(curringAdd(1)(2));
-
   return (
     <>
       <SeoTool
