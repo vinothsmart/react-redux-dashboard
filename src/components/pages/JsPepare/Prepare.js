aeccdf11567c5edc26714acc8a946ed5d8eaad67
vinothsmart <s.vinothkanna90@gmail.com>
2022-05-05 16:41:46 +0530

    map and foreach

--- src/components/pages/JsPepare/Prepare.js
+++ src/components/pages/JsPepare/Prepare.js
@@ -2,16 +2,18 @@ const Part1 = () => {
   // Map
   let a = [1, 2, 3, 4];
 
-  a.forEach((item, index) => {
-    console.log(item);
-    console.log(index);
-    console.log("----------");
+  let b = a.forEach((item, index) => {
+    return item;
   });
-  a.map((item, index) => {
-    console.log(item);
-    console.log(index);
-    console.log("----------");
+  let c = a.map((item, index) => {
+    return item;
   });
+  // Diff foreach and map
+  // foreach is loop the array not return anything
+  // map is return the new array
+
+  console.log(b);
+  console.log(c);
   return <div>Part1</div>;
 };
 
