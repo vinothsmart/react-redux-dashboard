22674beed77ebcdcc9a6db9d70598fc326ac7c08
vinothsmart <s.vinothkanna90@gmail.com>
2022-05-05 16:43:44 +0530

    filter

--- src/components/pages/JsPepare/Prepare.js
+++ src/components/pages/JsPepare/Prepare.js
@@ -1,19 +1,27 @@
 const Part1 = () => {
   // Map
-  let a = [1, 2, 3, 4];
+  const a = [1, 2, 3, 4];
 
-  let b = a.forEach((item, index) => {
+  const b = a.forEach((item, index) => {
     return item;
   });
-  let c = a.map((item, index) => {
+
+  const c = a.map((item, index) => {
     return item;
   });
+
+  const d = a.filter((item, index) => {
+    return item;
+  });
+
   // Diff foreach and map
   // foreach is loop the array not return anything
   // map is return the new array
-
+  // filter is return the also new array
   console.log(b);
   console.log(c);
+  console.log(d);
+
   return <div>Part1</div>;
 };
 
