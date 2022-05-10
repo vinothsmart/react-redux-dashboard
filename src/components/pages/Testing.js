bfc842d4f100b786f23097f12400e22961f92a29
vinothsmart <s.vinothkanna90@gmail.com>
2022-05-10 05:47:04 +0530

    rm calcu

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,20 +1,6 @@
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
-  const calCaluateDays = (calcData) => {
-    const currentDate = new Date();
-    const date1 = new Date(calcData);
-    const diff = Math.abs(date1.getTime() - currentDate.getTime());
-    const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
-    return diffDays;
-  };
-  // 2 days before
-  const daysBefore = calCaluateDays("2022-04-24");
-  console.log(daysBefore);
-
-  const numbers = [1, 2, 3, 4];
-  const result1 = numbers.reduce((acc, curr) => acc + curr);
-
   return (
     <>
       <SeoTool
@@ -25,7 +11,6 @@ export const Testing = () => {
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
-          <h1>Total sum {result1}</h1>
         </div>
       </div>
     </>
