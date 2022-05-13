efc56781aa7f9464841cfc7026042c5b3f5f9c43
vinothsmart <s.vinothkanna90@gmail.com>
2022-05-13 18:44:15 +0530

    some using

--- src/components/pages/JsPepare/Part2.js
+++ src/components/pages/JsPepare/Part2.js
@@ -2,13 +2,21 @@ import React from "react";
 
 const Part2 = () => {
   const arr = [1, 2, 3, 5, -1, 6, 3, 5, 11, 12];
-  let hasNegative = false;
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    if (arr[i] < 0) {
-      hasNegative = true;
-    }
-  }
+  // find array has negative number or not ?
+  // let hasNegative = false;
+  // for (let i = 0; i < arr.length; i++) {
+  //   console.log(arr[i]);
+  //   if (arr[i] < 0) {
+  //     hasNegative = true;
+  //     break;
+  //   }
+  // }
+
+  // console.log(hasNegative);
+
+  const hasNegative = arr.some((item) => {
+    return item < 0;
+  });
 
   console.log(hasNegative);
 
