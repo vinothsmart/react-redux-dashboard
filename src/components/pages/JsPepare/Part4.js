760a64a7c6b3d3099048eeee83f2f7214ab713d3
vinothsmart <s.vinothkanna90@gmail.com>
2022-05-30 12:43:03 +0530

    part4

--- src/components/pages/JsPepare/Part4.js
+++ src/components/pages/JsPepare/Part4.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const Part4 = () => {
   // Js coercion
-  console.log(1 + "1");
+  console.log(1 + "1"); //11
+  console.log(!0); //true
+
+  // other set  of example
+  console.log(Number("1")); //1
+  console.log(Number("1,2")); //2
+  console.log(Number([])); //3
+  console.log(Number([1])); //4
+  console.log(Number("a")); //5
   return <div>Part4</div>;
 };
 
