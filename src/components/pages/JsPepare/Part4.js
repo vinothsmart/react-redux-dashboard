2a245ca3534b1c98031651fbb5cc832b322d2c1d
vinothsmart <s.vinothkanna90@gmail.com>
2022-05-30 15:57:27 +0530

    string coercoin

--- src/components/pages/JsPepare/Part4.js
+++ src/components/pages/JsPepare/Part4.js
@@ -5,12 +5,20 @@ const Part4 = () => {
   console.log(1 + "1"); //11
   console.log(!0); //true
 
-  // other set  of example
+  // other set of example for number
   console.log(Number("1")); //1
   console.log(Number("1,2")); //2
   console.log(Number([])); //3
   console.log(Number([1])); //4
   console.log(Number("a")); //5
+
+  // other set of example for string
+  console.log(String(1)); //1
+  console.log(String({})); //2
+  console.log(String([])); //3
+  console.log(String(undefined)); //4
+  console.log(String(null)); //5
+
   return <div>Part4</div>;
 };
 
