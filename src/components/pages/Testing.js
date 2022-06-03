98f3449dc2ed72c3bd4665cd9136e281a0e95617
vinothsmart <s.vinothkanna90@gmail.com>
2022-06-03 16:51:59 +0530

    xxx

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -2,27 +2,16 @@ import { useCallback, useState } from "react";
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
-  const names = [
-    {
-      name: "Ganesh",
-    },
-    {
-      name: "Vinoth",
-    },
-    {
-      name: "Ganesh",
-    },
-  ];
+  const vinoth = [1, 2, 3, 4, 5];
+  const n = 0;
+  // output 3,4,5,1,2
 
-  // remove duplicate
-  const uniqueNames = names.reduce((acc, curr) => {
-    if (!acc.includes(curr.name)) {
-      acc.push(curr.name);
-    }
-    return acc;
-  }, []);
+  const getNValue = vinoth.splice(0, n);
+  const getRemainingValues = vinoth.splice(vinoth.length - n, n);
 
-  console.log(uniqueNames);
+  const result = [...getRemainingValues, ...getNValue];
+
+  console.log(result);
 
   return (
     <>
