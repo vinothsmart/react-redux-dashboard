129c479866143e75dd4638f74256be3d08c3d37f
vinothsmart <s.vinothkanna90@gmail.com>
2022-06-03 16:50:21 +0530

    form

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,6 +1,27 @@
+import { useCallback, useState } from "react";
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [showUserData, setShowUserData] = useState(false);
+
+  const handleChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setUserData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }, []);
+
+  const handleSumbit = useCallback(() => {
+    setShowUserData(true);
+  }, []);
+
   return (
     <>
       <SeoTool
@@ -11,6 +32,17 @@ export const Testing = () => {
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
+          <input type="text" name="userName" onChange={handleChange} />
+          <input type="text" name="userAge" onChange={handleChange} />
+          <input type="text" name="userPhone" onChange={handleChange} />
+          <button onClick={handleSumbit}>add</button>
+          {showUserData && (
+            <div>
+              <p>Name : {userData.name}</p>
+              <p>Age : {userData.userAge}</p>
+              <p>Phone : {userData.userPhone}</p>
+            </div>
+          )}
         </div>
       </div>
     </>
