2879c9acc338b489b6c52906f26b58ae4be34255
vinothsmart <s.vinothkanna90@gmail.com>
2022-06-11 15:57:15 +0530

    pothon

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,16 +1,45 @@
+import { useCallback } from "react";
+import { useState } from "react";
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
-  const name = "vinothkanaa";
+  // var a = [ a, b, c,d,b,c,e,c]
 
-  // reverse the name
-  const reverseName = name
-    .split("")
-    .map((item) => item)
-    .reverse()
-    .join("");
+  // const arrayList = ["a", "b", "c", "d", "b", "c", "e", "c"];
 
-  console.log(reverseName);
+  // const result = arrayList.reduce((acc, curr) => {
+  //   if (acc[curr]) {
+  //     acc[curr]++;
+  //   } else {
+  //     acc[curr] = 1;
+  //   }
+  //   return acc;
+  // }, {});
+
+  // console.log(result);
+
+  // var a = [15, 27, 43, 65];
+
+  // var second = 0;
+
+  // for (var i = 0; i < a.length - 1; i++) {
+  //   if (a[i] > second) {
+  //     second = a[i];
+  //   }
+  // }
+
+  // console.log(second);
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  const handleDecremClick = useCallback(() => {
+    setCount(count - 1);
+  }, [count]);
+
   return (
     <>
       <SeoTool
@@ -21,6 +50,9 @@ export const Testing = () => {
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
+          <h2>{count}</h2>
+          <button onClick={handleClick}> Increment</button>
+          <button onClick={handleDecremClick}> Decrement</button>
         </div>
       </div>
     </>
