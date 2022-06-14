720a81075caff84d303a47543ec26204054a8279
vinothsmart <s.vinothkanna90@gmail.com>
2022-06-15 05:22:48 +0530

    submit

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,37 +1,6 @@
-import { useCallback } from "react";
-import { useState } from "react";
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
-  // Write React JS Application to create Two TextField  One Name Other City and One Submit Button ,When user enter the value Name,City and CliCk Submit Button then value print it
-
-  const [values, setValues] = useState({
-    name: "",
-    city: "",
-  });
-
-  const handleChange = useCallback(
-    (e) => {
-      setValues({
-        ...values,
-        [e.target.name]: e.target.value,
-      });
-      // setValues((prev) => ({
-      //   ...prev,
-      //   [e.target.name]: e.target.value,
-      // }));
-    },
-    [values]
-  );
-
-  const handleSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      console.log(values);
-    },
-    [values]
-  );
-
   return (
     <>
       <SeoTool
@@ -42,19 +11,6 @@ export const Testing = () => {
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="city"
-            placeholder="City"
-            onChange={handleChange}
-          />
-          <button onClick={handleSubmit}>Submit</button>
         </div>
       </div>
     </>
