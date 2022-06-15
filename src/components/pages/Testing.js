5fb617c66b1a9dd5c1d627db00840ab88870812f
vinothsmart <s.vinothkanna90@gmail.com>
2022-06-15 06:15:39 +0530

    rm dupliate of array

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,6 +1,16 @@
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
+  const array = ["🐑", 1, 2, "🐑", "🐑", 3];
+  // remove duplicate
+  // const unique = [...new Set(array)];
+  // another remove duplicate
+  const unique = array.filter((item, index) => {
+    console.log("index", index);
+    console.log("aindex", array.indexOf(item));
+    return array.indexOf(item) === index;
+  });
+
   return (
     <>
       <SeoTool
@@ -11,6 +21,9 @@ export const Testing = () => {
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
+          {unique.map((item, index) => {
+            return <div key={index}>{item}</div>;
+          })}
         </div>
       </div>
     </>
