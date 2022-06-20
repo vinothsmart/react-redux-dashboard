1005b80f8f6d2a7cd434da6a3b8974cfce4a1d18
vinothsmart <s.vinothkanna90@gmail.com>
2022-06-20 06:44:22 +0530

    hexaware

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,12 +1,30 @@
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
-  const names = ["vinoth", "kanna", "Santhosh", "Selva"];
+  const { a, b, ...c } = { c: 1, d: 2, b: 1 };
+  console.log(a, b, c);
+  // var y = 1;
+  // if (function f() {​}​) {​
+  // y += typeof f;
+  // }
 
-  // get longest name
-  const longestName = names.reduce((a, b) => {
-    return a.length > b.length ? a : b;
-  }, "");
+  // console.log(y)​
+
+  // multiply(5)(6);
+
+  // console.log(add(1,2,3,4,5));
+
+  const arr = [1, 5, 3, -1, 10];
+  const forEachRes = arr.forEach((num) => ++num);
+  console.log("forEachRes", forEachRes);
+
+  const arr1 = [1, 5, 3, -1, 10];
+  const mapRes = arr.map((num) => ++num);
+  console.log(mapRes);
+  const arr2 = [1, 5, 3, -1, 10];
+  const filterRes = arr.filter((num) => ++num);
+
+  console.log(filterRes);
 
   return (
     <>
@@ -18,7 +36,6 @@ export const Testing = () => {
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
-          <h3>Longest Name : {longestName}</h3>
         </div>
       </div>
     </>
