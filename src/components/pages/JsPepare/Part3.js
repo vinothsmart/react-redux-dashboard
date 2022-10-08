450997bb97347af766d20a5c8939d3a4d6704a33
vinothsmart <s.vinothkanna90@gmail.com>
2022-10-08 12:17:45 +0530

    unused func commented

--- src/components/pages/JsPepare/Part3.js
+++ src/components/pages/JsPepare/Part3.js
@@ -3,28 +3,28 @@ import React from "react";
 const Part3 = () => {
   // Higher order function (HOF)
 
-  function myfun(arg) {
-    arg();
-  }
+  // function myfun(arg) {
+  //   arg();
+  // }
 
-  function myfun() {
-    return function () {};
-  }
+  // function myfun() {
+  //   return function () {};
+  // }
 
   //function assigned variable
-  var myfunction = function () {};
+  // var myfunction = function () {};
 
   //object assigned variable
-  var obj = {
-    myfunction: function () {},
-  };
+  // var obj = {
+  //   myfunction: function () {},
+  // };
 
   //array assigned variable
-  var arr = [function () {}, function () {}];
+  // var arr = [function () {}, function () {}];
   // function called as argument
-  arr.map(function (item) {
-    console.log(item);
-  });
+  // arr.map(function (item) {
+  //   console.log(item);
+  // });
 
   return <div>Part3</div>;
 };
