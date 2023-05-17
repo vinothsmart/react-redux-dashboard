e8352d8d96a6f661171b6bb9f7b0bf4cd1b5da5b
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-17 13:10:21 +0530

    warning hided

--- src/components/pages/TCSChallange/Challange2.js
+++ src/components/pages/TCSChallange/Challange2.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 const Challange2 = () => {
-  console.log(1 < 2 < 3); // true  ----> correct answer ----> true
-  console.log(3 > 2 > 1); // false ----> correct answer ----> true
-  console.log(10 == [10]); // flase ----> correct answer ----> true
-  console.log(10 == [[[[[[[10]]]]]]]); // false ----> correct answer ----> true
-  console.log(0.1 + 0.2); // 0.10.2 ----> correct answer ----> 0.30000000000000004
-  console.log(0.1 + 0.2 == 0.3); // false ----> correct answer ----> false
-  console.log(10 + "10"); //20 ----> correct answer ----> 1010
-  console.log(10 - "10"); //0 ----> correct answer ----> 0
-  console.log([1, 2] + [3, 4]); // [1, 2, 3, 4]; ----> correct answer ----> 1,23,4
-  console.log(NaN === NaN); //true ----> correct answer ----> false
+  // console.log(1 < 2 < 3); // true  ----> correct answer ----> true
+  // console.log(3 > 2 > 1); // false ----> correct answer ----> true
+  // console.log(10 == [10]); // flase ----> correct answer ----> true
+  // console.log(10 == [[[[[[[10]]]]]]]); // false ----> correct answer ----> true
+  // console.log(0.1 + 0.2); // 0.10.2 ----> correct answer ----> 0.30000000000000004
+  // console.log(0.1 + 0.2 == 0.3); // false ----> correct answer ----> false
+  // console.log(10 + "10"); //20 ----> correct answer ----> 1010
+  // console.log(10 - "10"); //0 ----> correct answer ----> 0
+  // console.log([1, 2] + [3, 4]); // [1, 2, 3, 4]; ----> correct answer ----> 1,23,4
+  // console.log(NaN === NaN); //true ----> correct answer ----> false
 
   const userPrivileges = ["user", "user", "user", "admin"];
   const containsAdmin = userPrivileges.some((element, index) => {
