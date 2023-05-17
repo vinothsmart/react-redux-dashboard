2a3746808b14f65dc7a7e249892e298db485de49
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-17 16:15:36 +0530

    CTS interview

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,6 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { SeoTool } from "../elements";
 
 export const Testing = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      // const response = await fetch(
+      //   "https://jsonplaceholder.typicode.com/todos"
+      // );
+      axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+        setTodos(res.data);
+      });
+    };
+    fetchApi();
+  }, []);
+
+  const handleDelete = (id) => {
+    // const id = e.target.value;
+
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <SeoTool
@@ -11,6 +34,12 @@ export const Testing = () => {
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
+          {todos.map((todo) => (
+            <div>
+              <h1>{todo.title}</h1>
+              <button onClick={() => handleDelete(todo.id)}>delete</button>
+            </div>
+          ))}
         </div>
       </div>
     </>
