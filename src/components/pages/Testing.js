3d515dd1cbec141c88c1964befbc66e109d31849
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-29 15:13:35 +0530

    get only keys

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,4 +1,13 @@
 export const Testing = () => {
+  const object = { a: { b: { c: { d: "foo" } } } };
+
+  const firstkey = Object.keys(object)[0];
+  const secondkey = Object.keys(object[firstkey])[0];
+  const thirdkey = Object.keys(object[firstkey][secondkey])[0];
+  const fourthkey = Object.keys(object[firstkey][secondkey][thirdkey])[0];
+
+  console.log([firstkey, secondkey, thirdkey, fourthkey]);
+
   return (
     <>
       <div className="container">
