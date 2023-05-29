458bb60d9a4fd85985bdb6ccefa1db89cbf0c0f2
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-29 15:39:49 +0530

    rm

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,30 +1,4 @@
 export const Testing = () => {
-  const object = { a: { b: { c: { d: "foo" } } } };
-
-  const firstkey = Object.keys(object)[0];
-  const secondkey = Object.keys(object[firstkey])[0];
-  const thirdkey = Object.keys(object[firstkey][secondkey])[0];
-  const fourthkey = Object.keys(object[firstkey][secondkey][thirdkey])[0];
-
-  console.log([firstkey, secondkey, thirdkey, fourthkey]);
-
-  // same as function above
-  const keys = Object.keys(object).reduce((acc, key) => {
-    acc.push(key);
-    // second need to inner object key here
-    const innerKey = Object.keys(object[key])[0];
-    acc.push(innerKey);
-    // third need to inner object key here
-    const innerInnerKey = Object.keys(object[key][innerKey])[0];
-    acc.push(innerInnerKey);
-    // fourth need to inner object key here
-    const innerInnerInnerKey = Object.keys(
-      object[key][innerKey][innerInnerKey]
-    )[0];
-    acc.push(innerInnerInnerKey);
-    return acc;
-  }, []);
-  console.log(keys);
   return (
     <>
       <div className="container">
