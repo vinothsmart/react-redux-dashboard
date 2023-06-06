08087a6b923cd7c5d5109f4523cfe438d815d237
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-06 16:52:50 +0530

    useState ad usecallback in react

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,11 +1,41 @@
-export const Testing = () => {
-  return (
-    <>
+// export const Testing = () => {
+//   return (
+//     <>
+//       <div className="container">
+//         <div className="py-4">
+//           <h1> Testing Page</h1>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+import React, { Component } from "react";
+
+export class Testing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "React",
+    };
+  }
+  updateName() {
+    this.setState({ name: "ClassCompoent2" });
+  }
+  handleName = this.updateName.bind(this);
+  render() {
+    return (
       <div className="container">
         <div className="py-4">
-          <h1> Testing Page</h1>
+          <h1> Test Page </h1>
+          <p>Hi {this.state.name}</p>
+          <button onClick={() => this.setState({ name: "ClassCompoent1" })}>
+            Onclick
+          </button>
+          <button onClick={this.handleName}>Onclick2</button>
         </div>
       </div>
-    </>
-  );
-};
+    );
+  }
+}
+
+export default Testing;
