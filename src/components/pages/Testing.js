54a21c4692546f63c14b64f3601a736c34a4f689
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-09 15:57:35 +0530

    Testing project

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,41 +1,11 @@
-// export const Testing = () => {
-//   return (
-//     <>
-//       <div className="container">
-//         <div className="py-4">
-//           <h1> Testing Page</h1>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-import React, { Component } from "react";
-
-export class Testing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "React",
-    };
-  }
-  updateName() {
-    this.setState({ name: "ClassCompoent2" });
-  }
-  handleName = this.updateName.bind(this);
-  render() {
-    return (
+export const Testing = () => {
+  return (
+    <>
       <div className="container">
         <div className="py-4">
-          <h1> Test Page </h1>
-          <p>Hi {this.state.name}</p>
-          <button onClick={() => this.setState({ name: "ClassCompoent1" })}>
-            Onclick
-          </button>
-          <button onClick={this.handleName}>Onclick2</button>
+          <h1> Testing Page</h1>
         </div>
       </div>
-    );
-  }
-}
-
-export default Testing;
+    </>
+  );
+};
