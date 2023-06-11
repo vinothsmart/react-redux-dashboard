ee809d6a6ce4518891e3842a5fb36662c6c60883
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-11 10:10:01 +0530

    get keys another method

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,4 +1,11 @@
 export const Testing = () => {
+  const data = { a: { b: { c: { d: "foo" } } } };
+  const firstKey = Object.keys(data)[0];
+  const secondKey = Object.keys(data.a);
+  const thirdKey = Object.keys(data.a.b);
+  const fourthKey = Object.keys(data.a.b.c);
+  const listArray = [...firstKey, ...secondKey, ...thirdKey, ...fourthKey];
+  console.log(listArray);
   return (
     <>
       <div className="container">
