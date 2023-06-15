1982750516785306e3d825ce71ce461c1121e1bf
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-15 18:29:20 +0530

    disbale and text update usig event

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,9 +1,21 @@
 export const Testing = () => {
+  function handleClickMe(event) {
+    console.log("clicked");
+    // do not use 'this', no dom querying
+    console.log(event.target);
+
+    // event.target.innerText = "Clicked";
+    // disable button
+    event.target.disabled = true;
+    // update button text
+    event.target.innerText = "Clicked";
+  }
   return (
     <>
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
+          <button onClick={handleClickMe}> Click Me</button>
         </div>
       </div>
     </>
