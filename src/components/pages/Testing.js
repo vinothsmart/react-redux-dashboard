1b474db707c2f843f0e7064dbfe30af694e8d155
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-19 10:09:22 +0530

    back to normal func

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,26 +1,9 @@
 export const Testing = () => {
-  const hanldeAddSkill = () => {
-    const skillList = document.getElementById("skillList");
-    const skillInput = document.querySelector("input");
-    const newSkill = document.createElement("li");
-    newSkill.innerHTML = skillInput.value;
-    skillList.appendChild(newSkill);
-    skillInput.value = "";
-  };
-
   return (
     <>
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
-          <input type="text" placeholder="Enter Skill" />
-
-          <button onClick={hanldeAddSkill()}>Add Skill</button>
-
-          <ul id="skillList">
-            <li>HTML</li>
-            <li>CSS</li>
-          </ul>
         </div>
       </div>
     </>
