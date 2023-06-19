ba3530ae7267a237c0efdcd5af52b6976e0523b3
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-19 13:21:40 +0530

    using key

--- src/components/pages/JsChallange/Objects.js
+++ src/components/pages/JsChallange/Objects.js
@@ -3,6 +3,7 @@ import React from "react";
 const Objects = () => {
   const accessObjectProp = (obj) => obj.country;
   const accessObjectofTwo = (obj) => obj["prop-2"];
+  const accessObjectThree = (obj, key) => obj[key];
   return (
     <div>
       <center>
@@ -14,6 +15,22 @@ const Objects = () => {
         <h1>Accessing object properties two</h1>
         <p>{accessObjectofTwo({ one: 1, "prop-2": 2 })}</p>
         <p>{accessObjectofTwo({ "prop-2": "two", prop: "test" })}</p>
+      </center>
+
+      <center>
+        <h1>Accessing object properties three</h1>
+        <p>
+          {accessObjectThree(
+            { continent: "Asia", country: "Japan" },
+            "continent"
+          )}
+        </p>
+        <p>
+          {accessObjectThree(
+            { country: "Sweden", continent: "Europe" },
+            "country"
+          )}
+        </p>
       </center>
     </div>
   );
