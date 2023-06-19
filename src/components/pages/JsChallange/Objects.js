30f7c15f107742e24e72a06a3b627ac78a3584a2
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-19 13:18:41 +0530

    accessingoftwo

--- src/components/pages/JsChallange/Objects.js
+++ src/components/pages/JsChallange/Objects.js
@@ -2,12 +2,18 @@ import React from "react";
 
 const Objects = () => {
   const accessObjectProp = (obj) => obj.country;
+  const accessObjectofTwo = (obj) => obj["prop-2"];
   return (
     <div>
       <center>
         <h1>Accessing Object Porperty</h1>
         <p>{accessObjectProp({ continent: "Asia", country: "Japan" })}</p>
         <p>{accessObjectProp({ country: "Sweden", continent: "Europe" })}</p>
+      </center>
+      <center>
+        <h1>Accessing object properties two</h1>
+        <p>{accessObjectofTwo({ one: 1, "prop-2": 2 })}</p>
+        <p>{accessObjectofTwo({ "prop-2": "two", prop: "test" })}</p>
       </center>
     </div>
   );
