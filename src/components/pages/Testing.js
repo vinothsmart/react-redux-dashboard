3ee767a51ba5cf31e9a437b893118cc4102a9feb
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-23 14:47:49 +0530

    backto normal

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,40 +1,9 @@
-import axios from "axios";
-import { useCallback, useState, useEffect, Fragment } from "react";
-
-const GITHUB_USERS_API = "https://api.github.com/users";
-
 export const Testing = () => {
-  const [users, setUsers] = useState([]);
-  const fetchUsers = async () => {
-    try {
-      const result = await axios.get(GITHUB_USERS_API);
-      setUsers(result.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const fetchUsersData = useCallback(() => {
-    fetchUsers();
-  }, []);
-
-  useEffect(() => {
-    fetchUsersData();
-  }, [fetchUsersData]);
   return (
     <>
       <div className="container">
         <div className="py-4">
           <h1> Testing Page</h1>
-          {users.map((user) => {
-            return (
-              <Fragment key={user.id}>
-                <div className="card-header">
-                  <h4>{user.login}</h4>
-                </div>
-              </Fragment>
-            );
-          })}
         </div>
       </div>
     </>
