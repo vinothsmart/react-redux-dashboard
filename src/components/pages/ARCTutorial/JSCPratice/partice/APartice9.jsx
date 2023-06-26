dd1c35737aae5a0d8b0d96debdd62aa5f2abdc15
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-26 13:10:27 +0530

    sorting in object

--- src/components/pages/ARCTutorial/JSCPratice/partice/APartice9.jsx
+++ src/components/pages/ARCTutorial/JSCPratice/partice/APartice9.jsx
@@ -7,6 +7,25 @@ const APartice9 = () => {
     (value, index) => Ids.indexOf(value) === index
   );
   console.log(unqiueValues);
+  const persons = [
+    { id: 1, name: "John", phone: "23" },
+    { id: 2, name: "Jane", phone: "23" },
+    { id: 1, name: "Johnny", phone: "56" },
+    { id: 4, name: "Alice", phone: "67" },
+  ];
+
+  const uniquePersonsById = [
+    ...new Map(persons.map((item) => [item.id, item])).values(),
+  ];
+
+  console.log(uniquePersonsById);
+
+  const uniquePersonsByPhone = [
+    ...new Map(persons.map((item) => [item.phone, item])).values(),
+  ];
+
+  console.log(uniquePersonsByPhone);
+
   return <div>APartice9</div>;
 };
 
