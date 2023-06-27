c305247b490e2af14b7c0c8431a56ec7833ecd3b
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-27 12:42:11 +0530

    list the item

--- src/components/pages/ARCTutorial/JSCPratice/JSChallange.jsx
+++ src/components/pages/ARCTutorial/JSCPratice/JSChallange.jsx
@@ -1,7 +1,29 @@
-import APartice18 from "./partice/APartice18";
+import APartice19 from "./partice/APartice19";
 
 const JSChallange = () => {
-  return <APartice18 />;
+  const data = {
+    student: [
+      {
+        id: 20,
+        firstName: "John",
+        lastName: "Doe",
+      },
+      {
+        id: 2,
+        firstName: "Jane",
+        lastName: "Doe",
+      },
+      {
+        id: 5,
+        firstName: "John",
+        lastName: "Smith",
+      },
+    ],
+  };
+
+  const result = data.student.filter((item) => (item.id > 5 ? item : null));
+  console.log(result);
+  return <APartice19 />;
 };
 
 export default JSChallange;
---
+++ src/components/pages/ARCTutorial/JSCPratice/partice/APartice19.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const APartice19 = () => {
+  return <div>APartice19</div>;
+};
+
+export default APartice19;
