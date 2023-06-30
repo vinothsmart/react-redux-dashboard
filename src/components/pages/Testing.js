ba75a6d225e4791a4df9e62f59822a4abd6641f2
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-30 13:00:53 +0530

    back to normal

--- src/components/pages/Testing.js
+++ src/components/pages/Testing.js
@@ -1,26 +1,9 @@
-import { useEffect, useState } from "react";
-
 export const Testing = () => {
-  // currentTime is the state variable
-  const time = new Date().toLocaleTimeString();
-  const [currentTime, setCurrentTime] = useState(time);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      // setCurrentTime(currentTime);
-      setCurrentTime(new Date().toLocaleTimeString());
-    }, 1000);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [currentTime]);
-
   return (
     <>
       <div className="container">
         <div className="py-4">
           <h1>Testing</h1>
-          <div>{currentTime} </div>
         </div>
       </div>
     </>
