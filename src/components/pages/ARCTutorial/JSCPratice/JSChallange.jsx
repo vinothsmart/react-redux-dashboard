cd492562fe847cf33416463f5478c86e5885158b
vinothsmart <s.vinothkanna90@gmail.com>
2023-07-13 12:07:14 +0530

    reverse the array

--- src/components/pages/ARCTutorial/JSCPratice/JSChallange.jsx
+++ src/components/pages/ARCTutorial/JSCPratice/JSChallange.jsx
@@ -1,7 +1,30 @@
-import APartice25 from "./partice/APartice25";
+import APartice26 from "./partice/APartice26";
 
 const JSChallange = () => {
-  return <APartice25 />;
+  const students = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+  const newStudents = students.slice().reverse();
+  console.log(students);
+  console.log(newStudents);
+
+  // sort and reverse array without mutating original array
+  const sortStudents = students.slice().sort().reverse();
+  console.log(sortStudents);
+
+  // another way
+  const sortStudents2 = [...students].sort().reverse();
+  console.log(sortStudents2);
+
+  // another way
+  const sortStudents3 = [...students].sort((a, b) => {
+    if (a > b) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  console.log(sortStudents3);
+
+  return <APartice26 />;
 };
 
 export default JSChallange;
---
+++ src/components/pages/ARCTutorial/JSCPratice/partice/APartice26.jsx
@@ -0,0 +1,9 @@
+const APartice26 = () => {
+  return (
+    <div>
+      <h1> APartice25</h1>
+    </div>
+  );
+};
+
+export default APartice26;
