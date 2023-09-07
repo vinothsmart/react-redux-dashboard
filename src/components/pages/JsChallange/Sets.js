c8b2134640c6050f3380844c9bffb4a144a18b34
vinothsmart <s.vinothkanna90@gmail.com>
2023-09-07 11:21:35 +0530

    union

--- src/components/pages/JsChallange/Sets.js
+++ src/components/pages/JsChallange/Sets.js
@@ -5,6 +5,32 @@ const Sets = () => {
     return set.has(val);
   };
 
+  //   const convertSetToArray = (set) => {
+  //     return [...set];
+  //   };
+
+  //   console.log(convertSetToArray(new Set([1, 2, 3])));
+  //   console.log(convertSetToArray(new Set([123])));
+  //   console.log(convertSetToArray(new Set(["1", "2", "3"])));
+  //   console.log(convertSetToArray(new Set("123")));
+
+  //   const getUnionSet = (a, b) => {
+  //     return new Set([...a, ...b]);
+  //   };
+
+  // another way
+  const getUnionSet = (a, b) => {
+    const result = new Set(a);
+    b.forEach((el) => result.add(el));
+    return result;
+  };
+
+  console.log(getUnionSet(new Set("123"), new Set("234")));
+  console.log(getUnionSet(new Set([1, 2, 3]), new Set([3, 4, 5])));
+  console.log(
+    getUnionSet(new Set([false, false, NaN]), new Set([true, true, NaN]))
+  );
+
   return (
     <>
       <center>
@@ -15,6 +41,12 @@ const Sets = () => {
           {checkValuePresent(new Set(["1", "2", "3"]), "2") ? "true" : "false"}
         </p>
         <p>{checkValuePresent(new Set("123"), "2") ? "true" : "false"}</p>
+      </center>
+      <center>
+        <h1>Convert a Set to Array</h1>
+      </center>
+      <center>
+        <h1>Get union of two sets</h1>
       </center>
     </>
   );
