877f4b635f05179afc983ee82bb63c4854b6470e
vinothsmart <s.vinothkanna90@gmail.com>
2024-08-05 09:52:07 +0530

    Refactor daysUpper variable in APartice14.jsx

--- src/components/pages/ARCTutorial/JSCPratice/partice/APartice14.jsx
+++ src/components/pages/ARCTutorial/JSCPratice/partice/APartice14.jsx
@@ -15,8 +15,14 @@ const APartice14 = () => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
-  const daysUpper = days.map(firstLetterUpper);
+  const daysUpper = days.map((day) => firstLetterUpper(day));
   console.log(daysUpper);
+
+  // another way
+  const daysUpper2 = days.map(
+    (day) => day.charAt(0).toUpperCase() + day.substring(1)
+  );
+  console.log(daysUpper2);
 
   return <div>APartice14</div>;
 };
